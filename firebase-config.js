d304768a2000ec903747ed57777811060a835b9f
sajan123456789 <sajanpandit585@gmail.com>
2026-07-17 18:34:48 +0530

    Add Firebase configuration to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+export const firebaseConfig = {
+  apiKey: "AIzaSyBm-B0nhpfBt776iDZJuQRplNrOhy9xdiY", 
+  authDomain: "careersteps-afa5d.firebaseapp.com",
+  projectId: "careersteps-afa5d",
+  storageBucket: "careersteps-afa5d.firebasestorage.app",
+  messagingSenderId: "197033973372",
+  appId: "1:197033973372:web:b5c6ad50bf8f68aee5dbf9"
+};
